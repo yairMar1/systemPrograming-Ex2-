6d218ea65cdaedc469c286278c632d549f801c0b
yairMar1 <156588104+yairMar1@users.noreply.github.com>
2024-02-08 12:08:32 +0200

    Add files via upload
    8.2.24 12:07

--- my_mat.c
+++ my_mat.c
@@ -8,7 +8,6 @@ void initialize(int matrix[columns][rows]) {
 
     for (int i = 0; i < columns; i++) {
         for (int j = 0; j < rows; j++) {
-            //printf("Enter the value for matrix[%d][%d]: ", i, j);
             scanf("%d", &matrix[i][j]);
         }
     }
@@ -19,23 +18,22 @@ void path(int matrix[columns][rows]) {
     int c = 0;
     int r = 0;
 
-    //printf("Enter which vertices you want to check");
     scanf("%d" "%d" , &c ,&r);
 
-    for (int i = 0; i < columns; i++){
-        for (int j = 0; j < rows; j++){
-            if(i == j){matrix[i][j] = INT_MAX;}
-            if(matrix[i][j] == 0){matrix[i][j] = INT_MAX;}
-        }
-        
-    }
+    //for (int i = 0; i < columns; i++){
+      //  for (int j = 0; j < rows; j++){
+          //  if(i == j){matrix[i][j] = 0;}
+        //    if(matrix[i][j] == 0){matrix[i][j] = INT_MAX;}
+      //  }
+    //}
     
     for(int k=0 ; k < columns ; k++){
         for(int i=0 ; i < columns ; i++){
             for (int j = 0; j < rows; j++){
-                if (matrix[i][k] != INT_MAX && matrix[k][j] != INT_MAX && matrix[i][j] > matrix[i][k] + matrix[k][j]) {
-                    matrix[i][j] = matrix[i][k] + matrix[k][j];}
-            }
+                 if( i == j || matrix[i][j] == 0){matrix[i][j] = INT_MAX;}
+                 else if (matrix[i][k] == INT_MAX || matrix[k][j] == INT_MAX ){continue;}
+                 else if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {matrix[i][j] = matrix[i][k] + matrix[k][j];}
+             }
         }
     }
     if (matrix[c][r] != INT_MAX){printf("True\n");}
@@ -48,24 +46,21 @@ void shortestPath(int matrix[columns][rows]) {
     int c = 0;
     int r = 0;
 
-    //printf("Enter which vertices you want to check");
     scanf("%d" "%d" , &c ,&r);
 
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < columns; j++){
-            if(i == j){matrix[i][j] = INT_MAX;}
-            if(matrix[i][j] == 0){matrix[i][j] = INT_MAX;}
-        }
-        
-    }
+    //for (int i = 0; i < rows; i++){
+      //  for (int j = 0; j < columns; j++){
+        //    if(i == j){matrix[i][j] = 0;}
+          //  if(matrix[i][j] == 0){matrix[i][j] = INT_MAX;}
+      //  }
+    //}
     
     for(int k=0 ; k < rows ; k++){
         for(int i=0 ; i < rows ; i++){
             for (int j = 0; j < columns; j++){
-                 if (matrix[i][k] != INT_MAX && matrix[k][j] != INT_MAX && matrix[i][j] > matrix[i][k] + matrix[k][j]) {
-                    matrix[i][j] = matrix[i][k] + matrix[k][j];
-}
-
+                 if( i == j || matrix[i][j] == 0){matrix[i][j] = INT_MAX;}
+                 else if (matrix[i][k] == INT_MAX || matrix[k][j] == INT_MAX ){continue;}
+                 else if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {matrix[i][j] = matrix[i][k] + matrix[k][j];}
             }
         }
     }
